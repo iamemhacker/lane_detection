08cb051d862a4e0c0604d32d1c857c4be3fba21b
Elior Malul <elior@orcavision.company>
2025-03-23 15:38:44 +0200

    test

---
+++ tests/test_lane_detection.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import sys
+
+import numpy as np
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '..', 'src')))
+from lane_detection import detect  # noqa E402
+
+# TODO: replace.
+TEST_FILE = "/Users/eliormalul/work/auto-counter/_training_sets/fly-stands-multi/input.mp4"
+
+
+def test_lane_detect():
+    counter = 0
+    gen = detect(TEST_FILE)
+    while True:
+        try:
+            frame, lines = next(gen)
+            line_image = np.copy(frame)
+            for line in lines:
+                cv2.line(line_image, line[:2], line[2:], (0, 255, 0), 3)
+            counter += 1
+            cv2.imwrite(f"/tmp/frame-lined{counter}.png", line_image)
+            print(f"Frame {counter} written")
+        except StopIteration:
+            break
+        except Exception:
+            assert False
+
+    assert counter > 0
